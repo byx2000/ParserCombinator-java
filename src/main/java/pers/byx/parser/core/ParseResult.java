e1676078d838affca9bdd65482545ca1615a1595
byx2000 <416982860@qq.com>
2020-10-14 08:22:36 +0800

    重构ParseResult

--- src/main/java/pers/byx/parser/core/ParseResult.java
+++ src/main/java/pers/byx/parser/core/ParseResult.java
@@ -1,44 +1,33 @@
 package pers.byx.parser.core;
 
-public abstract class ParseResult
+public class ParseResult
 {
-    public abstract boolean isSuccess();
-    public abstract Sequence remain();
+    private final boolean success;
+    private final Sequence remain;
+
+    private ParseResult(boolean success, Sequence remain)
+    {
+        this.success = success;
+        this.remain = remain;
+    }
+
+    public boolean isSuccess()
+    {
+        return success;
+    }
+
+    public Sequence remain()
+    {
+        return remain;
+    }
 
     public static ParseResult success(Sequence remain)
     {
-        return new ParseResult()
-        {
-
-            @Override
-            public boolean isSuccess()
-            {
-                return true;
-            }
-
-            @Override
-            public Sequence remain()
-            {
-                return remain;
-            }
-        };
+        return new ParseResult(true, remain);
     }
 
     public static ParseResult fail(Sequence remain)
     {
-        return new ParseResult()
-        {
-            @Override
-            public boolean isSuccess()
-            {
-                return false;
-            }
-
-            @Override
-            public Sequence remain()
-            {
-                return remain;
-            }
-        };
+        return new ParseResult(false, remain);
     }
 }
